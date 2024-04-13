72ade94da3f9d8f538df69cb46a49e8c47d29a25
satpil <pillaisatish88@gmail.com>
2024-04-13 14:44:11 +0530

    feat: add health api

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const port = 3001; // You can use any port number you prefer
 app.get('/', (req, res) => {
   res.send('Hello John');
 });
+
+app.get('/health', (req, res) => {
+  res.send('success');
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
